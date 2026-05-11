36f21bf042211bbd1adb7871ec38051b7da88b6f
Dillon Draper <dillonbdraper@gmail.com>
2026-05-10 21:45:10 -0500

    Added release.ex working towards CI/CD

---
+++ release.ex
@@ -0,0 +1,24 @@
+defmodule FosBjj.Release do
+  @app :fos_bjj
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def sync_config do
+    load_app()
+    FosBjj.ConfigData.sync_all()
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+  end
+end
